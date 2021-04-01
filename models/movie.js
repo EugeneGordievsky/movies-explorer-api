2925aa8d244ddbfd6bd8149c0b671e06d7d3a7f9
Eugene Gordievsky <eugene.gordievsky@yandex.ru>
2021-04-01 21:29:29 +0300

    fix: movieId pattern

--- models/movie.js
+++ models/movie.js
@@ -56,14 +56,14 @@ const movieSchema = new mongoose.Schema({
     type: String,
     required: true,
     validate(name) {
-      return /^[А-Яа-яA-Za-z\s\W]{1,}$/.test(name);
+      return /^[А-Яа-я\s\w]{1,}$/.test(name);
     },
   },
   nameEN: {
     type: String,
     required: true,
     validate(name) {
-      return /^[A-Za-z\s\W]{1,}$/.test(name);
+      return /^[\s\w]{1,}$/.test(name);
     },
   },
 });
